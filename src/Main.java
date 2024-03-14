8fa5972e2bb3646739335ef3a43662893f5bcef3
DevJhojan <jhojan1806@gmail.com>
2024-03-14 07:41:41 -0500

    number pi ok

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import calculator.BasicOperations;
+import calculator.MyPi;
 public class Main {
     public static void main(String[] args) {
         BasicOperations calculated = new BasicOperations();
+        MyPi myPi = new MyPi();
         System.out.println(calculated.DataManagement("(5+3+4)/3"));
+        System.out.println(myPi.PI);
     }
 }
