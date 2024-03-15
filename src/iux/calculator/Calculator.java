56da29ed4c03cd2a9ddc94a6ad063237348b3081
DevJhojan <jhojan1806@gmail.com>
2024-03-15 08:01:14 -0500

    Click mode begin

--- src/iux/calculator/Calculator.java
+++ src/iux/calculator/Calculator.java
@@ -2,6 +2,8 @@ package iux.calculator;
 
 import op.calculator.*;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -10,6 +12,7 @@ public class Calculator extends JFrame {
     private JLabel description;
     private JButton btnChangeMode;
     public JPanel jpCalculatorMain;
+
     public Calculator(){
         this.actionsKey();
     }
@@ -23,17 +26,23 @@ public class Calculator extends JFrame {
             }
             @Override
             public void keyPressed(KeyEvent e) {
-                if(e.getKeyCode() == KeyEvent.VK_ENTER){
-                    this.operation= txtOperation.getText();
-                    System.out.println(this.operation);
-                    txtOperation.setText(Double.toString(this.basicOperations.DataManagement(operation)));
-                }
+                if(e.getKeyCode() == KeyEvent.VK_ENTER)
+                    txtOperation.setText(Double.toString(this.basicOperations.DataManagement(txtOperation.getText())));
             }
             @Override
             public void keyReleased(KeyEvent e) {
 
             }
         });
+        btnChangeMode.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+    }
+    private void sizeCalculator(Calculator calculator, boolean mode){
+        if(!mode) calculator.setSize(400, 200);
+        else calculator.setSize(300, 600);
     }
     public void start(){
         Calculator calculator = new Calculator();
